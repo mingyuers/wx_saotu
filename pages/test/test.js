7f18f1a880983f60e5639b2a658cb43ba14ec10a
fangm <>
2018-07-27 18:06:34 +0800

    update:add share&save

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,8 +1,7 @@
 var page = undefined;
 Page({
   data: {
-    imgData: [
-    ],
+    imgData: [],
     indicatorDots: false,
     autoplay: false,
     interval: 5000,
@@ -11,43 +10,44 @@ Page({
     current_name: '',
     currentTab: 0,
     shareData: {
-      title: 'ins照片墙',
+      title: '[有人@我]快来欣赏Instagram美图啦',
       desc: 'ins照片墙',
       path: '/pages/test/test'
     },
     doommData: []
   },
-  onLoad: function () {
+  onLoad: function() {
     page = this;
     this.add_one_data()
   },
-  changeEvent: function (e) {
+  changeEvent: function(e) {
     var current_page = e.detail.current + 1;
     var totle_page = this.data.imgData.length;
     console.log(current_page + "```" + totle_page)
     if ((totle_page - current_page) <= 5) {
       this.add_one_data()
     }
-    this.refreshCheckBoxData(current_page)
+    this.setData({
+      currentTab: e.detail.current
+    })
   },
 
-  add_one_data: function () {
+  add_one_data: function() {
     var that = this
     var old_data = that.data.imgData
     wx.request({
       url: 'https://api.betteridea.cn/wximg/',
-      success: function (res) {
+      success: function(res) {
         var new_data = old_data.concat(res.data.data)
         //        console.log(new_data);
         that.setData({
           imgData: new_data,
         })
-        that.refreshCheckBoxData(0)
       }
     })
     //    console.log(old_data)
   },
-  previewImage: function (e) {
+  previewImage: function(e) {
     console.log(e)
     //var that = this
     var url = this.data.imgData[this.data.currentTab].imgurl
@@ -56,14 +56,105 @@ Page({
       urls: [url] // 需要预览的图片http链接列表  
     })
   },
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     return this.data.shareData
   },
-  bindbt: function () {
-    console.log(this.data.doommData);
-    doommList.push(new Doomm("你是我的小苹果", Math.ceil(Math.random() * 200), Math.ceil(Math.random() * 5 + 5), getRandomColor()));
-    this.setData({
-      doommData: doommList
+  saveImg: function(e) {
+    wx.showLoading({
+      title: '正在保存...'
     })
+    var that = this
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.writePhotosAlbum']) {
+          wx.authorize({
+            scope: 'scope.writePhotosAlbum',
+            success() {
+              that.doSave()
+            },
+            fail() {
+              wx.hideLoading()
+              wx.showToast({
+                title: '保存失败，无权限',
+                duration: 2000
+              })
+            }
+          })
+        } else {
+          that.doSave()
+        }
+      },
+      fail(res) {
+        wx.hideLoading()
+        wx.showToast({
+          title: '保存失败',
+          duration: 2000
+        })
+      }
+    });
   },
+
+  doSave: function() {
+    wx.showLoading({
+      title: '正在保存...'
+    })
+    var imgurl = this.data.imgData[this.data.currentTab].imgurl
+    wx.downloadFile({
+      url: imgurl,
+      success: function(res) {
+        let path = res.tempFilePath
+        wx.saveImageToPhotosAlbum({
+          filePath: path,
+          success(res) {
+            console.log(res)
+            wx.hideLoading()
+            wx.showToast({
+              title: '保存成功',
+              duration: 2000
+            })
+          },
+          fail(res) {
+            console.log(res)
+            wx.hideLoading()
+            wx.showToast({
+              title: '保存失败',
+              duration: 2000
+            })
+          },
+          complete(res) {
+            console.log(res)
+          }
+        })
+      },
+      fail: function(res) {
+        console.log(res)
+      }
+    })
+  }
+  // saveImg:function(e) {
+  //   var imgurl = this.data.imgData[this.data.currentTab].imgurl
+  //   wx.getImageInfo({
+  //     src: imgurl,
+  //     success: function (res) {
+  //       console.log(res.path)
+  //       wx.getSetting({
+  //         success(res) {
+  //           if (!res.authSetting['scope.writePhotosAlbum']) {
+  //             wx.authorize({
+  //               scope: 'scope.writePhotosAlbum',
+  //               success() {
+  //                 wx.saveImageToPhotosAlbum({
+  //                   filePath: res.path,
+  //                   success(result) {
+  //                     console.log(result)
+  //                   }
+  //                 })
+  //               }
+  //             })
+  //           }
+  //         }
+  //       })
+  //     }
+  //   })
+  // }
 })
